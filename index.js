1be0e6eafb281e6acf4cf987cc62edc9666a0e7f
sarayu96 <sarayu96@yahoo.com>
2021-04-08 21:16:05 +0300

    Implementedwinner status

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ if (randomNumber1 === 2) {
   image1.setAttribute("src", "images/dice2.png");
 }
 if (randomNumber1 === 3) {
-image1.setAttribute("src", "images/dice3.png");
+  image1.setAttribute("src", "images/dice3.png");
 }
 if (randomNumber1 === 4) {
-image1.setAttribute("src", "images/dice4.png");
+  image1.setAttribute("src", "images/dice4.png");
 }
 if (randomNumber1 === 5) {
-image1.setAttribute("src", "images/dice5.png");
+  image1.setAttribute("src", "images/dice5.png");
 }
 if (randomNumber1 === 6) {
   image1.setAttribute("src", "images/dice6.png");
@@ -34,14 +34,25 @@ if (randomNumber2 === 2) {
   image2.setAttribute("src", "images/dice2.png");
 }
 if (randomNumber2 === 3) {
-image2.setAttribute("src", "images/dice3.png");
+  image2.setAttribute("src", "images/dice3.png");
 }
 if (randomNumber2 === 4) {
-image2.setAttribute("src", "images/dice4.png");
+  image2.setAttribute("src", "images/dice4.png");
 }
 if (randomNumber2 === 5) {
-image2.setAttribute("src", "images/dice5.png");
+  image2.setAttribute("src", "images/dice5.png");
 }
 if (randomNumber2 === 6) {
   image2.setAttribute("src", "images/dice6.png");
+}
+//change the value of h1 to display the winner
+let winnerStatus = document.querySelector("h1");
+if (randomNumber1 === randomNumber2) {
+  winnerStatus.innerHTML = "It's a draw. Try again!!!";
+}
+if (randomNumber1 > randomNumber2) {
+  winnerStatus.innerHTML = "Player 1 wins!!!";
+}
+if (randomNumber1 < randomNumber2) {
+  winnerStatus.innerHTML = "Player 2 wins!!!";
 }
