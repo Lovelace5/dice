945c7e694b1465270c260da496e59ec44a96aeff
sarayu96 <sarayu96@yahoo.com>
2021-04-08 20:50:52 +0300

    Made images to change asyncronously on refresh

--- index.js
+++ index.js
@@ -27,21 +27,21 @@ let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 let image2 = document.querySelector(".img2");
 
-if (randomNumber1 === 1) {
+if (randomNumber2 === 1) {
   image2.setAttribute("src", "images/dice1.png");
 }
-if (randomNumber1 === 2) {
+if (randomNumber2 === 2) {
   image2.setAttribute("src", "images/dice2.png");
 }
-if (randomNumber1 === 3) {
+if (randomNumber2 === 3) {
 image2.setAttribute("src", "images/dice3.png");
 }
-if (randomNumber1 === 4) {
+if (randomNumber2 === 4) {
 image2.setAttribute("src", "images/dice4.png");
 }
-if (randomNumber1 === 5) {
+if (randomNumber2 === 5) {
 image2.setAttribute("src", "images/dice5.png");
 }
-if (randomNumber1 === 6) {
+if (randomNumber2 === 6) {
   image2.setAttribute("src", "images/dice6.png");
 }
